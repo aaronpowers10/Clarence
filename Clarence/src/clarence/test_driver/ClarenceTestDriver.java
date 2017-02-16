28792069f880a61f3361f027b4fad5a5c72d8b60
aaronpowers10 <caaronpowers@gmail.com>
2017-02-16 11:29:51 -0600

    Update

--- Clarence/src/clarence/test_driver/ClarenceTestDriver.java
+++ Clarence/src/clarence/test_driver/ClarenceTestDriver.java
@@ -28,7 +28,7 @@ public class ClarenceTestDriver {
 
 	public static void main(String[] args) throws IOException {
 		DOE2DataProperties dataProperties = KeyReader.read("C:\\doe22\\EXE48r\\BDLKEY.BIN");
-
+		
 		try {
 			FileWriter output = new FileWriter(new File("BDLKEY.TXT"));
 			for (int i = 0; i < dataProperties.size(); i++) {
